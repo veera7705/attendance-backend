2df36861194025cc22a1598368981656241e4af2
veera7705 <veerasivareddy328@gmail.com>
2026-08-03 18:35:35 +0530

    login-playwright.js

--- login-playwright.js
+++ login-playwright.js
@@ -35,13 +35,13 @@ async function login(registerNo, password) {
 
     try {
        browser = await chromium.launch({
-       headless: true,
-       args: [
-           "--no-sandbox",
-           "--disable-setuid-sandbox",
-           "--disable-dev-shm-usage"
-        ]
-     });
+    headless: false,
+    args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage"
+    ]
+});
 
         const page = await browser.newPage();
         page.setDefaultTimeout(config.navTimeoutMs);
